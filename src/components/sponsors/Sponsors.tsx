753e7f04957c3cd493b09e2628a593d63a9a2ba2
Rachelle DeMan <demanr@mcmaster.ca>
2023-01-12 16:29:39 -0500

    chore: fixed ordering

--- src/components/sponsors/Sponsors.tsx
+++ src/components/sponsors/Sponsors.tsx
@@ -57,30 +57,35 @@ const Sponsors = () => {
         </div>
       </div>
       <div className="w-full relative flex justify-center items-center lg:px-44 lg:pb-32 flex-wrap flex-col">
-        <div className="w-1/5 lg:w-1/3 flex items-center justify-center pb-8">
-          <a href="https://www.fdmgroup.com/en-us/home-us/">
+        <div className="w-1/5 lg:w-1/3 flex items-center justify-center pb-8"></div>
+        <div className="flex justify-evenly items-center lg:gap-32 py-8">
+          <a
+            className="w-2/5 pb-8"
+            href="https://www.fdmgroup.com/en-us/home-us/"
+          >
             <img src={FDM} className="" alt="FDM logo"></img>
           </a>
-        </div>
-        <div className="flex justify-evenly items-center lg:gap-32 py-8">
+
           <a className="w-2/5" href="https://www.digitalocean.com/">
             <img src={DigitalOcean} alt="Digital Ocean logo"></img>
           </a>
+        </div>
+        <div className="flex justify-evenly items-center lg:gap-32 py-8">
           <a className="w-2/5" href="https://www.wolfram.com/language/">
             <img src={Wolfram} alt="Wolfram Language logo"></img>
           </a>
-        </div>
-        <div className="flex justify-evenly items-center lg:gap-32 py-8">
           <a className="w-2/5" href="https://www.verbwire.com/">
             <img src={Verbwire} alt="VerbWire Logo"></img>
           </a>
+        </div>
+        <div className="flex justify-evenly items-center lg:gap-32 py-8">
           <a className="w-2/5" href="https://www.stickeryou.com/">
             <img src={Stickeryou} alt="StickerYou Logo"></img>
           </a>
-          <a className="w-2/5" href="https://balsamiq.com/">
+          <a className="w-2/5 pb-8" href="https://balsamiq.com/">
             <img src={Balsamiq} alt="Balsamiq logo"></img>
           </a>
-        </div>
+        </div>{" "}
       </div>
     </div>
   );
